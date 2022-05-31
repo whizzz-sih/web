735396d40ef43da182939aa7a0af6cec1b69e5de
Aakash Shrivastava <69726390+aakzsh@users.noreply.github.com>
2022-05-31 17:55:01 +0530

    add navbar scroll

--- src/components/Navv.js
+++ src/components/Navv.js
@@ -1,8 +1,58 @@
 
 import './Navv.css'
 import logo from '../images/logo.png'
+import { scroller } from "react-scroll";
+
+
+
 
 function Navbarr(){
+ function scrollToHome () {
+   document.getElementById('home').style.fontWeight = 700;
+   document.getElementById('packages').style.fontWeight = 400;
+   document.getElementById('offer').style.fontWeight = 400;
+   document.getElementById('about').style.fontWeight = 400;
+    scroller.scrollTo('search-parent', {
+      duration: 800,
+      delay: 0,
+      smooth: "easeInOutQuart",
+    });
+  };
+  function scrollToOffer () {
+    document.getElementById('home').style.fontWeight = 400;
+    document.getElementById('packages').style.fontWeight = 400;
+    document.getElementById('offer').style.fontWeight = 700;
+    document.getElementById('about').style.fontWeight = 400;
+    scroller.scrollTo('xyz', {
+      duration: 800,
+      delay: 0,
+      smooth: "easeInOutQuart",
+    });
+  };
+  function scrollToPackages () {
+    document.getElementById('home').style.fontWeight = 400;
+    document.getElementById('packages').style.fontWeight = 700;
+    document.getElementById('offer').style.fontWeight = 400;
+    document.getElementById('about').style.fontWeight = 400;
+    scroller.scrollTo('packages-parent', {
+      duration: 800,
+      delay: 0,
+      smooth: "easeInOutQuart",
+
+    });
+  };
+  function scrollToAbout () {
+    document.getElementById('home').style.fontWeight = 400;
+    document.getElementById('packages').style.fontWeight = 400;
+    document.getElementById('offer').style.fontWeight = 400;
+    document.getElementById('about').style.fontWeight = 700;
+    scroller.scrollTo('about', {
+      duration: 800,
+      delay: 0,
+      smooth: "easeInOutQuart",
+
+    });
+  };
   return (
     <>
       <div class="nav">
@@ -21,10 +71,10 @@ function Navbarr(){
   </div>
   
   <div class="nav-links">
-    <a href="//github.io/jo_geek" target="_blank">HOME</a>
-    <a href="http://stackoverflow.com/users/4084003/" target="_blank">PACKAGES</a>
-    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">WHAT DO WE OFFER?</a>
-    <a href="https://codepen.io/jo_Geek/" target="_blank">ABOUT US</a>
+    <a onClick={ scrollToHome} className='navtext' id='home'>HOME</a>
+    <a onClick={ scrollToPackages} className='navtext' id='packages'>PACKAGES</a>
+    <a onClick={scrollToOffer} className='navtext' id='offer'>WHAT DO WE OFFER?</a>
+    <a onClick={scrollToAbout} className='navtext' id='about'>ABOUT US</a>
   </div>
 </div>
     </>
